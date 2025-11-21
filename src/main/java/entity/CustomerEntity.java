86f8a9caf3842540eb3754c7ee73fb965669fc52
kalharaweragoda <kalharaweragoda@gmail.com>
2025-11-21 21:15:19 +0530

    customerentity add

---
+++ src/main/java/entity/CustomerEntity.java
@@ -0,0 +1,29 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "customers")
+public class CustomerEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String email;
+
+    @Column(nullable = false, name = "phone_number", length = 15)
+    private String phoneNumber;
+}
